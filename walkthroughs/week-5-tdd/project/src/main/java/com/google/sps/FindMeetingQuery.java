7aa1f6bdd1238f484ce500f099f0573c53112214
Sofia Vega <sofiavega@google.com>
2020-07-14 17:59:14 -0600

    Complete FindMeetingQuery.java All tests in FindMeetingQueryTest.java passed

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,58 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> result = new ArrayList<TimeRange>();
+    boolean includeEvent;
+    boolean first = true;
+    if (request.getDuration() > TimeRange.getTimeInMinutes(23, 59)) {
+      return result;
+    }
+    if (events.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+    Event lastEvent = new Event("Not an event",
+    TimeRange.fromStartDuration(0, 30), Arrays.asList("Me"));
+    int lastTime;
+    for (Event event : events) {
+      includeEvent = false;
+      for (String person : request.getAttendees()) {
+        if (event.getAttendees().contains(person)) {
+          includeEvent = true;
+          break;
+        }
+      }
+      if (includeEvent) {
+        if (first) {
+          if (event.getWhen().start() >= request.getDuration()) {
+            result.add(TimeRange.fromStartDuration(0, event.getWhen().start()));
+          }
+          first = false;
+          lastEvent = event;
+        } else {
+          lastTime = (int) lastEvent.getWhen().start() + lastEvent.getWhen().duration();
+          if(event.getWhen().start()-lastTime >= request.getDuration()){
+            result.add(TimeRange.fromStartDuration(lastTime, event.getWhen().start() - lastTime));
+          }
+          if ((int) event.getWhen().start()+event.getWhen().duration() > lastTime) {
+            lastEvent = event;
+          }
+        }
+      }
+    }
+    if (first) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+    lastTime = (int) lastEvent.getWhen().start() + lastEvent.getWhen().duration();
+    if (lastTime < TimeRange.END_OF_DAY) {
+      result.add(TimeRange.fromStartEnd(lastTime, TimeRange.END_OF_DAY, true));
+    }
+    return result;
   }
 }
